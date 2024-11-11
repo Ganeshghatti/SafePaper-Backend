6bef3c6d3a4340bcb66f02f70db6bb509fbff91f
GaneshGhatti <ganeshghatti6@gmail.com>
2024-11-11 14:36:30 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const connectdatabase = async () => {
   try {
     // this is db from aniket mongodb account
     await mongoose.connect(
-      "mongodb+srv://ganesh:12@aidcircle.uvee4.mongodb.net/?retryWrites=true&w=majority&appName=aidcircle"
+      "mongodb+srv://ganeshghatti6:vH5QoAtQNMsIrjjG@safepaper.8x5px.mongodb.net/?retryWrites=true&w=majority&appName=SafePaper"
     );
 
     console.log("db connection successful");
